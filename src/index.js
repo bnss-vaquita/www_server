87b8b55013c88765440aff61fd7c5225fbaa07a5
Kristian Alvarez Jörgensen <krijor@kth.se>
2019-03-12 12:27:07 +0100

    Finish totp auth flow

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const login = require('./login');
+const notp = require('notp');
 
 const app = express();
 const ip = '127.0.1.3';
@@ -51,59 +52,171 @@ app.use(requireHTTPS);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-app.post('/login', (req, res) => {
+const token_req = (username, password, success, error) => {
+    const payload = JSON.stringify({
+        grant_type: "password",
+        username: username,
+        password: password,
+        client_id: "test_client",
+        client_secret: "secret"
+    });
+    const req_options = { ...auth_options,
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': payload.length
+        }
+    };
+
+    // Request a token
+    const request = https.request(req_options, (auth_res) => {
+        if (auth_res.statusCode != 200) {
+            error(auth_res.statusCode);
+        } else {
+            auth_res.on('data', (data) => {
+                const response = JSON.parse(data);
+                success(response);
+            });
+        }
+    });
+    request.write(payload);
+    request.end();
+    request.on('error', (e) => {
+        console.error(e);
+    });
+}
+
+const file_req = (username, token, success, error) => {
+    const req_options = {
+        ...rs_options,
+        path: `/${username}/files`,
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    };
+    // Retrive the list of files
+    const request = https.request(req_options, (rs_res) => {
+        if (rs_res.statusCode != 200) {
+            error(rs_res.statusCode);
+        } else {
+            rs_res.on('data', (data) => {
+                success(JSON.parse(data));
+            });
+        }
+    });
+    request.end();
+    request.on('error', (e) => {
+        console.error(e);
+    });
+}
+
+const secret_req = (username, token, success, error) => {
+    const req_options = {
+        ...rs_options,
+        path: `/${username}/key`,
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    };
+    // Retrive the key
+    const request = https.request(req_options, (rs_res) => {
+        if (rs_res.statusCode != 200) {
+            error(rs_res.statusCode);
+        } else {
+            rs_res.on('data', (data) => {
+                success(JSON.parse(data));
+            });
+        }
+    });
+    request.end();
+    request.on('error', (e) => {
+        console.error(e);
+    });
+
+}
+
+const totp = (key) => {
+};
+
+app.post('/', (req, res) => {
 	const cert = req.connection.getPeerCertificate();
     const username = req.body.username;
     const password = req.body.password;
+    // Authenticated certificate flow
     if(req.client.authorized && username === cert.subject.CN) {
-
-        const payload = JSON.stringify({
-            grant_type: "password",
-            username: username,
-            password: password,
-            client_id: "test_client",
-            client_secret: "secret"
-        });
-        const req_options = { ...auth_options,
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': payload.length
-            }
-        };
-        const request = https.request(req_options, (auth_res) => {
-            auth_res.on('data', (data) => {
-                const response = JSON.parse(data);
-                const req_options = {
-                    ...rs_options,
-                    path: `/${username}/files`,
-                    headers: {
-                        'Authorization': `Bearer ${response.access_token}`
+        // Request the token
+        token_req(
+            username,
+            password,
+            (t_response) => {
+                const token = t_response.access_token;
+                // Request the file
+                file_req(
+                    username,
+                    token,
+                    (f_response) => {
+                        res.send(f_response);
+                    },
+                    (error_code) => {
+                        res.status(error_code)
+                            .send("Login Failed!\n");
                     }
-                };
-                console.log(req_options);
-                const request = https.request(req_options, (rs_res) => {
-                    rs_res.on('data', (data) => {
+                )
+            },
+            (error) => {
+            res.status(auth_res.statusCode)
+                .send("Login failed");
+            }
+        );
 
-                        res.send(JSON.parse(data));
-                    });
-                });
-                request.end();
-                request.on('error', (e) => {
-                    console.error(e);
-                });
-
-            });
-        });
-        request.write(payload);
-        request.end();
-        request.on('error', (e) => {
-            console.error(e);
-        });
     }
     else {
-        res.send('Access Denied!');
-        console.log(cert.subject.CN);
-        console.log(username);
+        const totp = req.body.totp;
+
+        token_req(
+            username,
+            password,
+            (t_response) => {
+                const token = t_response.access_token;
+                secret_req(
+                    username,
+                    token,
+                    (s_response) => {
+                        const key = s_response.key;
+                        console.log(key);
+                        const login = notp.totp.verify(totp, key);
+                        console.log(login);
+
+                        if(login) {
+                            // Request the file
+                            file_req(
+                                username,
+                                token,
+                                (f_response) => {
+                                    console.log(f_response);
+                                    res.send(f_response);
+                                },
+                                (error_code) => {
+                                    res.status(error_code)
+                                        .send("Login Failed!\n");
+                                }
+                            )
+
+                        } else {
+                            res.status(400)
+                                .send("Login Failed!\n");
+                        };
+
+                    },
+                    (error_code) => {
+                        res.status(error_code)
+                            .send("Login Failed!\n");
+                    }
+                );
+            },
+            (error_code) => {
+                res.status(error_code)
+            }
+        );
     }
 
 });
--- src/login.js
+++ src/login.js
@@ -3,7 +3,7 @@ exports.totp = () => `
     <body>
     <center>
         <h2>Verify your identity with the code in your ACME app:</h2>
-        <form method="post" action="/2fa">
+        <form method="post" action="/">
             <div>
                 Username
                 <input name="username" type="text">
@@ -28,7 +28,7 @@ exports.login = (id) => `
     <body>
     <center>
         <h2>Welcome, ${id}! Insert your password:</h2>
-        <form method="post" action="/login">
+        <form method="post" action="/">
             <input name="username" value="${id}" type="hidden">
             <div>
                 <input name="password" type="password">
